3ee5bbece15089e98d59ed006ea6b65a91bde38f
Shinfreyr <fh.lamodiere@gmail.com>
2018-03-08 10:55:17 +0100

    Account Management view

--- project/view/frontend/accountManagementView.php
+++ project/view/frontend/accountManagementView.php
@@ -10,6 +10,19 @@
     while ($db = $request->fetch()) {
         echo    '<div class="accountManagement">
                     <img src="project/public/images/' . htmlspecialchars($db['avatar']) . '" />
+                    <button type="button" class="btn btn-info btn-lg btn-block">Modifier</button>
+                    <h2 class="accountPseudo" >Pseudo: ' . htmlspecialchars($db['pseudo']) . '</h2>
+                    <button type="button" class="btn btn-info btn-lg btn-block">Modifier</button>
+                    <h2 class="accountPseudo" >Prénom: ' . htmlspecialchars($db['firstName']) . '</h2>
+                    <button type="button" class="btn btn-info btn-lg btn-block">Modifier</button>
+                    <h2 class="accountPseudo" >Nom: ' . htmlspecialchars($db['lastName']) . '</h2>
+                    <button type="button" class="btn btn-info btn-lg btn-block">Modifier</button>
+                    <h2 class="accountPseudo" >Email: ' . htmlspecialchars($db['eMail']) . '</h2>
+                    <button type="button" class="btn btn-info btn-lg btn-block">Modifier</button>
+                    <h2 class="accountPseudo" >Mot de Passe</h2>
+                    <button type="button" class="btn btn-info btn-lg btn-block">Modifier</button>
+                    
+
                 </div>';
     }
 
